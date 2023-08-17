20ef6239a8b7e30773b98bb2e700fcfa914d5e5b
Sameer Singh <sameergr20@gmail.com>
2023-08-18 00:08:09 +0530

    Update index.js

--- English Dictionary/index.js
+++ English Dictionary/index.js
@@ -1,41 +1,40 @@
 const inputEl = document.getElementById("input");
-const infotextEL = document.getElementById("info-text");
-const meaningcontEl = document.getElementById("meaning-container");
+const infoTextEl = document.getElementById("info-text");
+const meaningContainerEl = document.getElementById("meaning-container");
 const titleEl = document.getElementById("title");
 const meaningEl = document.getElementById("meaning");
 const audioEl = document.getElementById("audio");
 
+async function fetchAPI(word) {
+  try {
+    infoTextEl.style.display = "block";
+    meaningContainerEl.style.display = "none";
+    infoTextEl.innerText = `Searching the meaning of "${word}"`;
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    const result = await fetch(url).then((res) => res.json());
 
-async function fetchAPI(word){
-
-    try {
-        infotextEL.style.display = "block";
-        meaningcontEl.style.display = "none";
-        infotextEL.innerText = `Searching the Meaning of "${word}"`;
-        const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
-        const result = await fetch(url).then((res)=>res.json());
-        
-        if (result.title){
-            infotextEL.style.display = "none";
-            titleEl.style.display
-            meaningcontEl.style.display = "block";
-            audioEl.style.display = "none";
-        }
-        
-        
-        audioEl.style.display="inline-flex";
-        titleEl.innerText= result[0].word;
-        meaningEl.innerText= result[0].meanings[0].definitions[0].definition;
-        
-        audioEl.src = result[0].phonetics[0].audio;
-        
-    } catch (error) {
-        console.log(error);
+    if (result.title) {
+      meaningContainerEl.style.display = "block";
+      infoTextEl.style.display = "none";
+      titleEl.innerText = word;
+      meaningEl.innerText = "N/A";
+      audioEl.style.display = "none";
+    } else {
+      infoTextEl.style.display = "none";
+      meaningContainerEl.style.display = "block";
+      audioEl.style.display = "inline-flex";
+      titleEl.innerText = result[0].word;
+      meaningEl.innerText = result[0].meanings[0].definitions[0].definition;
+      audioEl.src = result[0].phonetics[0].audio;
     }
+  } catch (error) {
+    console.log(error);
+    infoTextEl.innerText = `an error happened, try again later`;
+  }
 }
 
-inputEl.addEventListener("keyup", (e)=>{
-    if(e.target.value && e.key ==="Enter"){
-        fetchAPI(e.target.value)
-    }
-})
+inputEl.addEventListener("keyup", (e) => {
+  if (e.target.value && e.key === "Enter") {
+    fetchAPI(e.target.value);
+  }
+});
